03bb63f0d1118454883635c7875764aa1319f9bf
fwexq <97286384+fwexq@users.noreply.github.com>
2022-01-10 18:46:50 +0300

    Add files via upload

--- task_06.py
+++ task_06.py
@@ -4,39 +4,37 @@ class WrongNumberOfPlayersError(Exception):
 class NoSuchStrategyError(Exception):
     pass
 
-class Game:
-    def rps_game_winner(self, players_choice: list):
-        if len(players_choice) > 2:
-            raise WrongNumberOfPlayersError
+def rps_game_winner(array):
+    if len(array) > 2:
+        raise WrongNumberOfPlayersError
 
-        if players_choice[0][1].lower() == 'p' and players_choice[1][1].lower() == 'a':
-            raise NoSuchStrategyError
+    elif array[0][1].lower() == 'p' and array[1][1].lower() == 'a':
+        raise NoSuchStrategyError
 
-        if players_choice[0][1].lower() == 'r' and players_choice[1][1].lower() == 's':
-            return 'player1 R'
-        if players_choice[0][1].lower() == 's' and players_choice[1][1].lower() == 'r':
-            return 'player2 R'
+    elif array[0][1].lower() == 'r' and array[1][1].lower() == 's':
+        return 'player1 R'
+    elif array[0][1].lower() == 's' and array[1][1].lower() == 'r':
+        return 'player2 R'
 
-        if players_choice[0][1].lower() == 's' and players_choice[1][1].lower() == 'p':
-            return 'player1 S'
-        if players_choice[0][1].lower() == 'p' and players_choice[1][1].lower() == 's':
-            return 'player2 S'
+    elif array[0][1].lower() == 's' and array[1][1].lower() == 'p':
+        return 'player1 S'
+    elif array[0][1].lower() == 'p' and array[1][1].lower() == 's':
+        return 'player2 S'
 
-        if players_choice[0][1].lower() == 'r' and players_choice[1][1].lower() == 'p':
-            return 'player2 P'
-        if players_choice[0][1].lower() == 'p' and players_choice[1][1].lower() == 'r':
-            return 'player1 P'
+    elif array[0][1].lower() == 'r' and array[1][1].lower() == 'p':
+        return 'player2 P'
+    elif array[0][1].lower() == 'p' and array[1][1].lower() == 'r':
+        return 'player1 P'
 
-        if players_choice[0][1].lower() == 'r' and players_choice[1][1].lower() == 'r':
-            return 'player1 R'
-        if players_choice[0][1].lower() == 's' and players_choice[1][1].lower() == 's':
-            return 'player1 S'
-        if players_choice[0][1].lower() == 'p' and players_choice[1][1].lower() == 'p':
-            return 'player1 P'
+    elif array[0][1].lower() == 'r' and array[1][1].lower() == 'r':
+        return 'player1 R'
+    elif array[0][1].lower() == 's' and array[1][1].lower() == 's':
+        return 'player1 S'
+    elif array[0][1].lower() == 'p' and array[1][1].lower() == 'p':
+        return 'player1 P'
 
 
-game = Game()
-print(game.rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']])) #=> WrongNumberOfPlayersError
-print(game.rps_game_winner([['player1', 'P'], ['player2', 'A']])) #=> NoSuchStrategyError
-print(game.rps_game_winner([['player1', 'P'], ['player2', 'S']]))  # => 'player2 S'
-print(game.rps_game_winner([['player1', 'P'], ['player2', 'P']]))  # => 'player1 P'
+print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']])) #=> WrongNumberOfPlayersError
+print(rps_game_winner([['player1', 'P'], ['player2', 'A']])) #=> NoSuchStrategyError
+print(rps_game_winner([['player1', 'P'], ['player2', 'S']]))  # => 'player2 S'
+print(rps_game_winner([['player1', 'P'], ['player2', 'P']]))  # => 'player1 P'
